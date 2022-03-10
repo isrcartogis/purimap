4d98a62f766e7988a369af1b803b7c6648fb0427
Dror Bogin <Dror.Bogin@gmail.com>
2022-03-10 07:57:24 +0200

    changed basemap to mute stamen watercolor

--- js/index.js
+++ js/index.js
@@ -2,9 +2,16 @@ const OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
+var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
+});
 // Create a map object with the basemap, center and zoom.
 const map = L.map('map',{
-    layers: OpenStreetMap_Mapnik,
+    layers: Stamen_Watercolor,
     center: [ 31.983617, 34.7703552 ],
     zoom: 11,
 });
