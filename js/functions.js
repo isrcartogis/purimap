d671e0e3d83ec90346c720adea782792d2066973
Dror Bogin <Dror.Bogin@gmail.com>
2022-03-13 10:03:51 +0200

    added shuffle and restircted repeat

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
 let features = [];
+let usableFeatures = [];
 let userPoints = 0;
 let currentPolygonLayer = L.geoJson({
     "type": "FeatureCollection",
@@ -70,7 +71,7 @@ function selectQuestionOptions(features, polygon){
         let j = Math.floor(Math.random() * optionalFeatures.length);
         ids.push(optionalFeatures[j].properties.SEMEL_YISH)
     }
-    return ids;
+    return shuffle(ids);
     
 }
 function getNewRandoms(){
@@ -83,9 +84,9 @@ function getNewRandoms(){
     if(whatnow._map){
         map.removeControl(whatnow)
     }
-    polygon = selectRandomPolygon(features);
-    questionOptions = selectQuestionOptions(features,polygon);
-    center = selectRandomLocation(features);
+    polygon = selectRandomPolygon(usableFeatures);
+    questionOptions = selectQuestionOptions(usableFeatures,polygon);
+    center = selectRandomLocation(usableFeatures);
     newPolygon = polygonToMarker(center.geometry.coordinates, polygon);
     newBounds = turf.bboxPolygon(turf.bbox(newPolygon))
     newLBounds = L.geoJson(newBounds).getBounds()
@@ -99,6 +100,7 @@ function getNewRandoms(){
 function getAnswer(e){
     if(e.target.id == polygon.properties.SEMEL_YISH){
         userPoints += 1
+        usableFeatures = usableFeatures.filter(x => x.properties.SEMEL_YISH != polygon.properties.SEMEL_YISH)
     }
     if(currentQuestions._map){
         map.removeControl(currentQuestions)
@@ -109,12 +111,29 @@ function getAnswer(e){
     whatnow = L.control.continue({score:userPoints});
     whatnow.addTo(map);
 }
-
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 async function onMapLoad(){
     const response = await fetch('set20k.fgb')
     for await (const f of flatgeobuf.deserialize(response.body)){
         feature = f;
         features.push(feature)
+        usableFeatures.push(feature)
     }   
     polygon = selectRandomPolygon(features);
     questionOptions = selectQuestionOptions(features,polygon);
